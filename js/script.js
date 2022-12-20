ca72904345a3e72500788d993aea8499316d5cf0
DarioDC95 <dario_95@live.com>
2022-12-20 18:52:54 +0100

    Exercise done with Bonusses

--- js/script.js
+++ js/script.js
@@ -3,7 +3,56 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            todolist: [
+                {
+                    text: "have breakfast",
+                    done: false,
+                },
+                {
+                    text: "follow Boolean's Lessons",
+                    done: false,
+                },
+                {
+                    text: "have lunch",
+                    done: false,
+                },
+                {
+                    text: "do the assigned afternoon exercise",
+                    done: false,
+                },
+                {
+                    text: "have dinner",
+                    done: false,
+                },
+            ],
+            new_text: undefined,
         }
-    }
+    },
+    methods: {
+        eraseTodo(index) {
+            this.todolist.splice(index, 1);
+        },
+        addTask() {
+            let new_textOK = this.new_text
+            if(this.new_text == undefined) {
+                this.new_text = undefined
+            }
+            else {
+                let todo = {
+                    text: new_textOK,
+                    done: false,
+                };
+                this.todolist.push(todo);
+            }
+            this.new_text = undefined;
+        },
+        completeTask(index) {
+            if(this.todolist[index].done == false) {
+                this.todolist[index].done = true;
+            }
+            else {
+                this.todolist[index].done = false;
+            }
+        },
+    } 
 }).mount('#app')
